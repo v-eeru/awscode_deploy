04051632617d65bb4cc05175a25b7dd50601e9ed
v-eeru <133241025+v-eeru@users.noreply.github.com>
2024-09-06 20:24:06 +0530

    Update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if sudo service nginx status | grep -q "active (running)"; then
-  echo "Nginx is already running."
-else
-  echo "Starting Nginx..."
-  sudo service nginx start
-fi
+sudo service nginx start
